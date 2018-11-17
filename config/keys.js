cb2b441f8d5bbd4be228c57f226b82975777ddf5
Austaino <c3ono32@yahoo.com>
2018-11-17 11:07:43 -0800

    Still working on this Initial Project Version

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
-  mongoURI: "mongodb://austaino:latitude123@ds263493.mlab.com:63493/naijconnect"
+  mongoURI:
+    "mongodb://austaino:latitude123@ds263493.mlab.com:63493/naijconnect",
+  secretOrKey: "secret"
 };
